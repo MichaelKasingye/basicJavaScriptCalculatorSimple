88721bd3e467cf166d7dc218665c23436d966977
KASINGYE <michaelkasingye@gmail.com>
2019-07-11 20:33:47 +0300

    Added more calculation functions in javascript

--- main.js
+++ main.js
@@ -5,9 +5,43 @@ function doSum()
            
              var sumfirstNumberSecondNumber =  firstNumber + secondNumber;
            
-             document.getElementById("sumfirstNumberSecondNumber").value = sumfirstNumberSecondNumber;
+             document.getElementById("ansfirstNumberSecondNumber").value = sumfirstNumberSecondNumber;
            }
-          
-     //onblur='doSum()' 
+           //onblur='doSum()' 
            // When you leave the input field, a function is triggered  
+           //subtraction
+           function doSub() 
+            {
+             var firstNumber = parseInt(document.getElementById("firstNumber").value, 10);
+             var secondNumber = parseInt(document.getElementById("secondNumber").value, 10);
            
+             var subfirstNumberSecondNumber =  firstNumber - secondNumber;
+           
+             document.getElementById("ansfirstNumberSecondNumber").value = subfirstNumberSecondNumber;
+           }
+            //multiplcation
+            function doMultiplication() 
+            {
+             var firstNumber = parseInt(document.getElementById("firstNumber").value, 10);
+             var secondNumber = parseInt(document.getElementById("secondNumber").value, 10);
+           
+             var mulfirstNumberSecondNumber =  firstNumber * secondNumber;
+           
+             document.getElementById("ansfirstNumberSecondNumber").value = mulfirstNumberSecondNumber;
+           }
+           //Division
+            function doDiv() 
+            {
+             var firstNumber = parseInt(document.getElementById("firstNumber").value, 10);
+             var secondNumber = parseInt(document.getElementById("secondNumber").value, 10);
+           
+             var divfirstNumberSecondNumber =  firstNumber / secondNumber;
+           
+             document.getElementById("ansfirstNumberSecondNumber").value = divfirstNumberSecondNumber;
+           }
+             //Clear
+             function clear() 
+             {
+              
+            }
+            
